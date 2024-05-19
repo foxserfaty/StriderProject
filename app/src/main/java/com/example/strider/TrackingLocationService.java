6b9d59a406cbef7b26b9db87fd06c75040f6cabe
luukhaihung <125259299+luukhaihung22028288@users.noreply.github.com>
2024-05-19 22:46:51 +0700

    update TrackingLocationService

--- app/src/main/java/com/example/strider/TrackingLocationService.java
+++ app/src/main/java/com/example/strider/TrackingLocationService.java
@@ -3,11 +3,11 @@ package com.example.strider;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Binder;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,24 +23,10 @@ public class TrackingLocationService extends Service {
 
     private static final String TAG = "TrackingLocationService";
     private FusedLocationProviderClient fusedLocationClient;
-    private LocationRequest locationRequest;
     private LocationCallback locationCallback;
     private static final int DEFAULT_INTERVAL = 2000;
-    private final IBinder binder = new LocalBinder();
+
     private Location currentLocation;
-
-    public class LocalBinder extends Binder {
-        public TrackingLocationService getService() {
-            return TrackingLocationService.this;
-        }
-    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -51,10 +37,7 @@ public class TrackingLocationService extends Service {
     private void createLocationCallback() {
         locationCallback = new LocationCallback() {
             @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
+            public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 List<Location> locations = locationResult.getLocations();
                 Location bestLocation = locations.get(0);
@@ -63,7 +46,6 @@ public class TrackingLocationService extends Service {
                         bestLocation = location;
                     }
                 }
-                currentLocation = bestLocation;
                 Intent intent = new Intent("LocationUpdate");
                 intent.putExtra("latitude", bestLocation.getLatitude());
                 intent.putExtra("longitude", bestLocation.getLongitude());
@@ -74,7 +56,7 @@ public class TrackingLocationService extends Service {
     }
 
     private void startLocationUpdates() {
-        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, DEFAULT_INTERVAL).build();
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, DEFAULT_INTERVAL).build();
         try {
             fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
         } catch (SecurityException e) {
@@ -98,7 +80,11 @@ public class TrackingLocationService extends Service {
         super.onDestroy();
     }
 
-    public Location getCurrentLocation() {
-        return currentLocation;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
+
+
 }
